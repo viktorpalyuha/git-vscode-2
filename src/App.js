ad76b216b517fcb553e53eadf9124cf08abdbaa9
Viktor Palyuha <viktorpalyuha@gmail.com>
2020-03-04 11:25:44 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Hello</div>
+        <h2>Hello</h2>
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
+
+export default App;
